a3be4590247d6c9addc77ed2957060ba47a9d9a9
sanpérez <spizzle313@gmail.com>
2023-08-13 13:45:25 -0400

    bugs

--- set_vel_and_move_sitl.py
+++ set_vel_and_move_sitl.py
@@ -5,8 +5,6 @@
 ################## IMPORTS ##################
   #########################################
 
-
-
 import time
 import socket
 import math
@@ -18,14 +16,17 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
 
 
 
+
+
+
   ##################################
 ############## FUNCTIONS #############
   ##################################
 
 
-
-# capable of launching and connecting to a virtual drone or the real thing
-def connectDrone(sitlInstance):
+# Capable of launching and connecting to a virtual drone, or the real thing.
+# If sitlInstance = True, then connect to sitl, o/w connect to RPi + Pixhawk.
+def connectToDrone(sitlInstance):
 
     if sitlInstance:
         vehicle = connect('udp:127.0.0.1:14550', wait_ready=True)
@@ -33,25 +34,30 @@ def connectDrone(sitlInstance):
         vehicle = connect('/dev/ttyAMA0', baud = 57600, wait_ready = True)
 
     return vehicle
+# connectDrone()
 
 
 
-# arm the drone and start props
+# Arms the drone and starts spinning the props, iff the drone
+# passes all pre-flight safety checks and has a good gps signal.
 def armDrone(vehicle):
 
+    # Check if armable, which depends on pre-flight checks and gps lock.
+    # If not armable, then run pre-flight checks again.
     if not vehicle.is_armable:
-        # begin the arming phase
+
+        # Run pre-flight checks.
         while not vehicle.is_armable:
             print("Waiting for drone to become armable...")
             time.sleep(1)
+        
+        # Passed all pre-flight checks.
         print("Vehicle is now armable!")
 
-    # once armable, enter desired flight mode
+    # Once armable, arm i.e. start motors.
     print("Initiating arm stage...")
-    
     vehicle.armed = True
     while not vehicle.armed:
-	
         print("Waiting for drone to become armed...")
         time.sleep(1)
 	
@@ -63,7 +69,8 @@ def armDrone(vehicle):
 
 
 
-# switch flight modes
+# Flight mode setter. Send your vehicle and a desired 
+# flight mode and this method will enable it. 
 def setMode(vehicle, flightMode):
 
     print("Entering", flightMode, "mode now...")
@@ -79,31 +86,43 @@ def setMode(vehicle, flightMode):
 
 
 
+# Simple lift-off method that utilizes the .alt attribute
+# and the DK f(x) simple_takeoff. It sends an elevate 
+# command and countinuously checks the attribute until
+# the desired height is reached.
 def takeoff(vehicle, targetHeight):
-    # make sure you are in guided mode!
+
+    
     # vehicle.mode = VehicleMode('GUIDED')
+
+    # IDK WHY THE FOLLOWING 'ARM' BLOCK IS THERE. THE DRONE 
+    # SHOULD ALREADY BE ARMED WHEN WE GET HERE, NO?
+    # TYPE HERE WHEN YOU REMEMBER!
+
+    '''
     if not vehicle.is_armable:
         # begin the arming phase
         while not vehicle.is_armable:
             print("Waiting for drone to become armable...")
             time.sleep(1)
         print("Vehicle is now armable!")
+    '''
 
+    # NOTE: Guided mode is required for most DK stuff
     setMode(vehicle, "GUIDED")
-
     armDrone(vehicle)
     
-    # take off to desired height
+    # take off to the desired height
     vehicle.simple_takeoff(targetHeight)
 
     while True:
         
+        # +/- .5 meters is an acceptable range
         acceptableHeight = targetHeight * 0.95
         currHeight = vehicle.location.global_relative_frame.alt
-
-        # +/- .5 meters is an acceptable range
         print("Current Altitude: ", currHeight)
 
+        # Once within an acceptable height, terminate
         if currHeight >= acceptableHeight:
             break
         time.sleep(1)
@@ -116,21 +135,45 @@ def takeoff(vehicle, targetHeight):
 
 
 
-# send velocity message (movement commands)
+# send velocity message (movement commands) in the drone's frame
 def send_local_ned_velocity(vehicle, vx, vy, vz):
-    # MAVLink message in the drone's frame (local)
+        
+    # MAVLink message in the local/drone's frame, where forward is the drone's heading
 	msg = vehicle.message_factory.set_position_target_local_ned_encode(
-		0,
-		0, 0,
-		mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
-		0b0000111111000111, # velocity bitmask
-		0, 0, 0,            # position
-		vx, vy, vz,         # velocity
-		0, 0, 0,            # acceleration
-		0, 0)
+		0,                                          #
+		0, 0,                                       #
+		mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,  # local frame
+		0b0000111111000111,                         # velocity bitmask
+		0, 0, 0,                                    # position
+		vx, vy, vz,                                 # velocity
+		0, 0, 0,                                    # acceleration
+		0, 0
+    )
+        
 	vehicle.send_mavlink(msg)
 	vehicle.flush()
 # send_local_ned_velocity()
+
+
+
+# send velocity message (movement commands) in a global frame
+def send_global_ned_velocity(vehicle, vx, vy, vz):
+
+    # MAVLink message in the global frame, where forward is true North
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,                                   #
+        0, 0,                                #
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # global frame
+        0b0000111111000111,                  # type_mask w/ only speed enabled
+        0, 0, 0,                             # position
+        vx, vy, vz,                          # velocity
+        0, 0, 0,                             # acceleration
+        0, 0                                 # yaw_rate
+    )
+
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+# send_global_ned_velocity()
 
 
 # NOTE: velocity commands must be resent every one second to keep
@@ -184,16 +227,56 @@ def moveRight(vehicle):
 
 
 
+# head north (+x), relative to Earth
+def moveForward_G(vehicle):
+    i = 0   # each iteration moves the drone
+    while (i < 12):
+        send_global_ned_velocity(vehicle, 1, 0, 0)
+        print("Moving forward now!")
+        time.sleep(1)
+        i += 1
+
+# head south (-x), relative to Earth
+def moveBackwards_G(vehicle):
+    i = 0   # each iteration moves the drone
+    while (i < 6):
+        send_global_ned_velocity(vehicle, -1, 0, 0)
+        print("Moving backwards now!")
+        time.sleep(1)
+        i += 1
+
+# head west (-y), relative to Earth
+def moveLeft_G(vehicle):
+    i = 0   # each iteration moves the drone
+    while (i < 6):
+        send_global_ned_velocity(vehicle, 0, -1, 0)
+        print("Moving to the left, now!")
+        time.sleep(1)
+        i += 1
+
+# head east (+y), relative to Earth
+def moveRight_G(vehicle):
+    i = 0   # each iteration moves the drone
+    while (i < 12):
+        send_global_ned_velocity(vehicle, 0, 1, 0)
+        print("Moving to the right, now!")
+        time.sleep(1)
+        i += 1
+    print("GLOBAL MISSION COMPLETE!!")
+
 ############# MAIN #############
 
 
 if __name__=='__main__':
 
+
+    # YOU SHOULD PUT A TRY-CATCH BLOCK HERE! (PER DK DOC)
+
     # set to false when field testing the real thing
     sitlInstance = True
 
     # connect to the drone
-    vehicle = connectDrone(sitlInstance)
+    vehicle = connectToDrone(sitlInstance)
 
     # print drone data
     #fetchAndPrintAttributes(vehicle)
@@ -219,10 +302,20 @@ if __name__=='__main__':
     # elevate and hover
     takeoff(vehicle, desiredHeight)
 
-    moveForward(vehicle)
+    # Cross maneuver in the local frame
+    '''moveForward(vehicle)
     moveBackwards(vehicle)
     moveLeft(vehicle)
-    moveRight(vehicle)
+    moveRight(vehicle)'''
+
+    # Now we try the same manuevers in the global frame
+    print("Now entering the global frame!!!!!")
+    print("Now entering the global frame!!!!!")
+    print("Now entering the global frame!!!!!")
+    moveForward_G(vehicle)
+    moveBackwards_G(vehicle)
+    moveLeft_G(vehicle)
+    moveRight_G(vehicle)
 
     # land the drone (and dont change altitude)
     vehicle.parameters['RTL_ALT'] = 0
